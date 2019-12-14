d983fa53fbe12ffcdd29b9d166a1e57f8bc9c39e
Anonymous <user@rivne.itstep.org>
2019-12-14 17:21:03 +0200

    First commit

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    [Serializable]
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public List<Mark> Marks { get; set; }
+        public Student()
+        {
+            Marks = new List<Mark>();
+        }
+        public override string ToString()
+        {
+            string Raiting = String.Empty;
+            foreach (var item in Marks)
+            {
+                Raiting = item + Environment.NewLine;
+            }
+            return $"---- {Name}, {Surname}, {Age}, {Raiting} ----";
+        }
+        public void AddMark(string subject, int value)
+        {
+            Marks.Add(new Mark
+            {
+                Subject = subject,
+                Raiting = value
+            });
+        }
+    }
+  
+    [Serializable]
+    public class Mark
+    {
+        public int Raiting { get; set; }
+        public string Subject { get; set; }
+        public override string ToString()
+        {
+            return $"{Subject} ----- ";
+        }
+       
+
+    }
+
+
+
+}
---
+++ Class2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary
+{
+    public class StudentService
+    {
+        private string path = "students.db";
+        public IEnumerable<Student> Students { get; set; }
+        public StudentService()
+        {
+            if (File.Exists(path))
+                Load();
+            Students = new List<Student>();
+        }
+        private void Load()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                Students = (List<Student>)bf.Deserialize(fs);
+            }
+        }
+        public void Add(Student st)
+        {
+            (Students as List<Student>).Add(st);
+        }
+
+        public void Remove(string Surname)
+        {
+            List<Student> temp = Students as List<Student>;
+            temp.Remove(temp.Find(st => st.Surname.Equals(Surname)));
+        }
+
+        public void Save()
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, Students);
+            }
+        }
+    }
+
+}
+
+
